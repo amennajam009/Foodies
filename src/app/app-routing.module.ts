64f05458612a52689b20231cefdc90f9b7ae9f83
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-03-31 15:06:11 +0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: ' ', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) },
+  { path: '', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) },
   { path: 'Admin-module', loadChildren: () => import('./admin-module/admin-module.module').then(m => m.AdminModuleModule) }, 
 { path: 'Main-module', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, 
 { path: 'managment-module', loadChildren: () => import('./managment-module/managment-module.module').then(m => m.ManagmentModuleModule) }];
