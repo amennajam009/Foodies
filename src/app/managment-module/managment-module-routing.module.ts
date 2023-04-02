62cc075571067b7b09a6ec3c9cce4938c4af0514
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-04-02 22:35:02 +0500

    add

--- src/app/managment-module/managment-module-routing.module.ts
+++ src/app/managment-module/managment-module-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManagmentModuleComponent } from './managment-module.component';
+import { AdminLoginComponent } from './admin-managment/admin-login/admin-login.component';
+import { AdminRegisterComponent } from './admin-managment/admin-register/admin-register.component';
+import { UserLoginComponent } from './user-managment/user-login/user-login.component';
+import { UserRegisterComponent } from './user-managment/user-register/user-register.component';
 
-const routes: Routes = [{ path: '', component: ManagmentModuleComponent }];
+const routes: Routes = [{ path: '', component: ManagmentModuleComponent,
+children:[
+  {path:'admin-login',component:AdminLoginComponent},
+  {path:'admin-register',component:AdminRegisterComponent},
+  {path:'user-login',component:UserLoginComponent},
+  {path:'user-register',component:UserRegisterComponent}
+] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
