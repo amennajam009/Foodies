4f919447a7f3cd127f9e6a7b5422268bd7c51cab
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-04-04 21:23:17 +0500

    add

--- src/app/admin-module/general-setting/general-setting/general-setting.component.ts
+++ src/app/admin-module/general-setting/general-setting/general-setting.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit ,ViewChild,ElementRef } from '@angular/core';
-import { FormBuilder,FormGroup,FormControl,ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder,FormGroup,FormControl,ReactiveFormsModule,Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { GeneralService } from 'src/app/shared/service/general.service';
 
@@ -25,10 +25,14 @@ export class GeneralSettingComponent implements OnInit {
 
   myFormModel(){
     this.fourcards=this._FormBuilder.group({
-      Headingone: new FormControl (''),
-      HeadingTwo: new FormControl (''),
-      HeadingThree: new FormControl (''),
-      HeadingFour: new FormControl (''),
+      Headingone: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      descriptionone: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      HeadingTwo: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      descriptionTwo: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      HeadingThree: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      descriptionThree: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      HeadingFour: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      descriptionFour: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)])
     })
   }
 
