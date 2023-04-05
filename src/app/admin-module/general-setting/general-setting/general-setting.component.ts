637daf4105f553e7f33fef74dd131c2c25e721aa
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-04-05 23:10:57 +0500

    add

--- src/app/admin-module/general-setting/general-setting/general-setting.component.ts
+++ src/app/admin-module/general-setting/general-setting/general-setting.component.ts
@@ -10,10 +10,11 @@ import { GeneralService } from 'src/app/shared/service/general.service';
   styleUrls: ['./general-setting.component.css']
 })
 export class GeneralSettingComponent implements OnInit {
-  Url='http://localhost:7070/'
-  newImageArray:any=[]
+
   fourcards:FormGroup | any;
-  @ViewChild('fileSelect') fileSelect:ElementRef|any
+  newImageArray:any=[]
+  @ViewChild('fileSelect') fileSelect:ElementRef|any;
+  
 
   constructor(private _FormBuilder:FormBuilder , private _General:GeneralService) { 
     this.myFormModel()
@@ -39,47 +40,29 @@ export class GeneralSettingComponent implements OnInit {
   }
 
 
-  Submitmyform(){
+  Submitmyform() {
     let FormValue = this.fourcards.value;
-    this._General.FourCardApi(FormValue).subscribe((resfrombackend)=>{
-      resfrombackend;
-      this.fourcards.reset();
-    })
-
-
-   
-    let  MultipartFormData=new FormData();
-    MultipartFormData.append('Headingone',this.fourcards.get('Headingone').value);
-    MultipartFormData.append(' descriptionone', this.fourcards.get(' descriptionone').value);
+  
+    let MultipartFormData = new FormData();
+    MultipartFormData.append('Headingone', this.fourcards.get('Headingone').value);
+    MultipartFormData.append('descriptionone', this.fourcards.get('descriptionone').value);
     MultipartFormData.append('HeadingTwo', this.fourcards.get('HeadingTwo').value);
     MultipartFormData.append('descriptionTwo', this.fourcards.get('descriptionTwo').value);
-    // MultipartFormData.append('Category', this.formproduct.get('Category').value);
     MultipartFormData.append('HeadingThree', this.fourcards.get('HeadingThree').value);
     MultipartFormData.append('descriptionThree', this.fourcards.get('descriptionThree').value);
-    // MultipartFormData.append('ProductMaterial', this.formproduct.get('ProductMaterial').value);
     MultipartFormData.append('HeadingFour', this.fourcards.get('HeadingFour').value);
     MultipartFormData.append('descriptionFour', this.fourcards.get('descriptionFour').value);
-    
-    this.newImageArray.forEach((imagedata:any)=>{
-  MultipartFormData.append('images',imagedata);
-    
-    })
-
-
-
-
-    
-    this._General.FourCardApi(MultipartFormData).subscribe((res:any)=>{
-      res;
-   
-      this.fourcards.reset()
-      
-      this.fileSelect.nativeElement.value=null
-      this.newImageArray=[]
-    
-    
-    })
   
+    this.newImageArray.forEach((imagedata: any) => {
+      MultipartFormData.append('images', imagedata);
+    });
+  
+    this._General.FourCardApi(MultipartFormData).subscribe((res: any) => {
+      res;
+      this.fourcards.reset();
+      this.fileSelect.nativeElement.value = null;
+      this.newImageArray = [];
+    });
   }
 
  
