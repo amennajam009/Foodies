389084c8d10c15c95d169836862a69ce70551ad9
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-04-30 18:20:29 +0500

    add

--- src/app/admin-module/menu/menu/menu.component.ts
+++ src/app/admin-module/menu/menu/menu.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef,ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup ,Validators } from '@angular/forms';
+import { MenuService } from 'src/app/shared/service/menu.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  foodcards:FormGroup|any;
+  getFoodImage:any
+  @ViewChild('fileSelect') fileSelect:ElementRef|any;
+  constructor(private _menuService:MenuService, private _FormBuilder:FormBuilder) { 
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
+
+  //FoodCardsModel
+  myFoodCardsModel(){
+    this.foodcards=this._FormBuilder.group({
+      FoodName:new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      FoodDescription:new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      FoodPrice: new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+    })
+}
+
+FoodImage(event:any){
+this.getFoodImage = event.target.files[0];
+}
+
+Submitmyfoodform(){
+  let MultipartFormData = new FormData();
+    MultipartFormData.append('FoodName', this.foodcards.get('FoodName').value);
+    MultipartFormData.append('FoodDescription', this.foodcards.get('FoodDescription').value);
+    MultipartFormData.append('Foodcard-image', this.getFoodImage);
+    
+   
+}
+
+
 
 }
