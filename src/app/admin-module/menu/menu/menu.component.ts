f37c40e2d77d594aa72aa43e79387dd03b3feaf0
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-04 15:57:06 +0500

    add

--- src/app/admin-module/menu/menu/menu.component.ts
+++ src/app/admin-module/menu/menu/menu.component.ts
@@ -9,10 +9,13 @@ import { MenuService } from 'src/app/shared/service/menu.service';
 })
 export class MenuComponent implements OnInit {
   foodcards:FormGroup | any;
+  breakfastfoodcards:FormGroup | any;
   getFoodImage:any
+  getBreakfastImage:any
   @ViewChild('fileSelect') fileSelect:ElementRef|any;
   constructor(private _menuService:MenuService, private _FormBuilder:FormBuilder) { 
   this.myFoodCardsModel();
+  this.myBreakfastcardModel();
   }
 
   ngOnInit(): void {
@@ -46,6 +49,28 @@ Submitmyfoodform(){
    
 }
 
+ myBreakfastcardModel(){
+  this.breakfastfoodcards=this._FormBuilder.group({
+      FoodName:new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      FoodDescription:new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      FoodPrice:new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+  })
+ }
 
+ BreakFastImage(event:any){
+  this.getBreakfastImage = event.target.files[0];
+ }
+ SubmitmyBreakfastform(){
+  let MultipleData = new FormData();
+  MultipleData.append('FoodName', this.breakfastfoodcards.get('FoodName').value);
+  MultipleData.append('FoodDescription', this.breakfastfoodcards.get('FoodDescription').value);
+  MultipleData.append('FoodPrice', this.breakfastfoodcards.get('FoodPrice').value);
+  MultipleData.append('breakcard-image', this.getBreakfastImage);
+
+ this._menuService.BreakfastFoodCardApi(MultipleData).subscribe((res:any)=>{
+  res;
+ })
+
+ }
 
 }
