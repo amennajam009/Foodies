704ab1d4e401022d5563991f5918d32667099ed2
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-23 01:02:55 +0500

    add

--- src/app/main-module/contact/contact/contact.component.ts
+++ src/app/main-module/contact/contact/contact.component.ts
@@ -2,26 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder ,Validators ,FormControl,FormGroup } from '@angular/forms';
 import { WhatsappService } from 'src/app/shared/service/whatsapp.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser'
+import { ContactService } from 'src/app/shared/service/contact.service';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  OrderTable:FormGroup | any
   phoneNumber: string = '+923064484061';
   WhatsAppForm:FormGroup | any;
-  constructor(private _whatsappService:WhatsappService , private _FormBuilder:FormBuilder ,private sanitizer: DomSanitizer) {
+  constructor(private _whatsappService:WhatsappService , private _FormBuilder:FormBuilder , private _contactservice:ContactService) {
     // this.myWhatsappForm();
+    this.Ordertablemodel();
    }
 
   ngOnInit(): void {
   }
  
+  Ordertablemodel(){
+    this.OrderTable = this._FormBuilder.group({
+      YourName: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      YourEmail: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      YourPhone: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      Date: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      Time: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      People: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+      Message: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
+    })
+  }
+
+  SubmitOrderTableform(){
+    const payLoad = this.OrderTable.value;
+    this._contactservice.UserBookedTable(payLoad).subscribe((res:any)=>{
+      res;
+      this.OrderTable.reset();
+    })
+  }
+
   
   generateWhatsAppLink(): string {
     const url = `https://api.whatsapp.com/send?phone=${encodeURIComponent(this.phoneNumber)}`;
     return url;
   }
+
+
+
+
   // myWhatsappForm(){
   //   this.WhatsAppForm = this._FormBuilder.group({
   //     WhatsAppMessage: new FormControl ('',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]),
