21ea093ed9ba0902b9da7f84f23d6330950d5848
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-22 16:39:04 +0500

    add

--- src/app/main-module/main-module.component.ts
+++ src/app/main-module/main-module.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-main-module',
   templateUrl: './main-module.component.html',
-  styleUrls: ['./main-module.component.css']
+  styleUrls: ['./main-module.component.css'],
+
 })
 export class MainModuleComponent implements OnInit {
 
