5ce02fe631ecad24f65a424d74d656b6cb2d25e4
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-06-11 18:59:28 +0500

    add

--- src/app/admin-module/foodies-analytics/foodies-analytics/foodies-analytics.component.ts
+++ src/app/admin-module/foodies-analytics/foodies-analytics/foodies-analytics.component.ts
@@ -33,7 +33,7 @@ export class FoodiesAnalyticsComponent implements OnInit {
   // imageDetailsArray: any[] = [];
  
   // ProductArray:any = []
-  Url='http://localhost:7070'
+  Url='http://localhost:7070/'
   constructor(private _General:GeneralService , private _menuService:MenuService, private Toaster:ToastrService) { }
 
   ngOnInit(): void {
