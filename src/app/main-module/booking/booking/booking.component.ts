23191b649d1750440bafd520cacf9e75cd887566
amennajam009 <amennajam7@gmail.com>
2023-08-29 19:32:07 +0500

    add

--- src/app/main-module/booking/booking/booking.component.ts
+++ src/app/main-module/booking/booking/booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CustomerbookingService } from 'src/app/shared/service/customerbooking.service';
 
 @Component({
   selector: 'app-booking',
@@ -7,31 +8,9 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
   @ViewChild('fileInput')fileInput!: ElementRef;
-  constructor() { }
+  constructor(private customerbookingservice:CustomerbookingService) { }
 
   ngOnInit(): void {
   }
   
-  readURL(input: any): void {
-    if (input.files && input.files[0]) {
-      const reader = new FileReader();
-      
-      reader.onload = (e: any) => {
-        const profilePic = document.querySelector('.profile_pic') as HTMLImageElement;
-        profilePic.src = e.target.result;
-      };
-
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-
-  onFileChange(event: any): void {
-    const input = event.target;
-    this.readURL(input);
-  }
-
-  onUploadButtonClick(): void {
-    this.fileInput.nativeElement.click();
-  }
-
 }
